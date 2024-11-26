7084b1bffa1fd43d3a77f2fe93b3029ff11335f5
Andreas-Sjogren-Furst <91537952+Andreas-Sjogren-Furst@users.noreply.github.com>
2024-11-26 18:44:20 +0100

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -8,33 +8,29 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # ham = 0, spam = 1
 
-
-
 train_data = pd.read_csv('sms_data/train.csv')
 test_data = pd.read_csv('sms_data/test.csv')
 
-
-vectorizer = TfidfVectorizer(max_features=5000)
+vectorizer = TfidfVectorizer()
 label_encoder = LabelEncoder()
-X_train = label_encoder.fit_transform(train_data['Tag'])
-X_train = X_train.reshape(-1,1)
-y_train = train_data['Text']
-y_train.str.lower()
+
+X_train = train_data['Text']
+X_train.str.lower()
+X_train = vectorizer.fit_transform(X_train)
+y_train = label_encoder.fit_transform(train_data['Tag'])
 
 
-X_test = label_encoder.fit_transform(test_data['Tag'])
-X_test = X_test.reshape(-1,1)
-y_test = test_data['Text']
+X_test = test_data['Text']
+X_test.str.lower()
+X_test = vectorizer.fit_transform(X_test)
+y_test = label_encoder.fit_transform(test_data['Tag'])
 
-#data['text'] = data['text'].str.lower()  # Lowercase
-#X = vectorizer.fit_transform(data['text'])
+print(X_test)
+print(X_train)
 
-model = RandomForestClassifier(n_estimators=100,random_state=42)
+model = RandomForestClassifier()
 model.fit(X_train,y_train)
 predictions = model.predict(X_test)
 
 print("Accuracy:",accuracy_score(y_test, predictions))
-
-
-
 
